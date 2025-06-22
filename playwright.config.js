85185c8182e13a329b6d0428e66ed2f35fda8260
Vinicios Virissimo <viniciosvirissimo@hotmail.com>
2025-06-22 20:58:29 -0300

    teste

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   reporter: [
     ['allure-playwright'],
+    ['list'],
+    ['html', {     
+      open: 'never',
+        }],
   ],
   outputDir: 'allure-results',
 });
